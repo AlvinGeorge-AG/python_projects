81592a1b01630b4412d1700e6b9c654ac5b83aa9
Alvin george <alvingeorge_@outlook.com>
2024-10-11 19:10:22 +0530

    bin_octal_hex_problems

---
+++ binary_hex_oct.py
@@ -0,0 +1,5 @@
+#to convert a decimal number to binary , hex,and octal equivalent using built in functions
+no=int(input("Enter an integer number :"))
+print("Binary :",bin(no))
+print("octal :",oct(no))
+print("hex :",hex(no))
