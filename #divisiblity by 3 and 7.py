66ca1eef0ad785211c49c86a55e1f4ddc03ba89a
Alvin george <alvingeorge_@outlook.com>
2024-10-12 19:14:42 +0530

    new_branch_anotherway_largest3num

--- #divisiblity by 3 and 7.py
+++ #divisiblity by 3 and 7.py
@@ -1,6 +1,6 @@
 #divisiblity by 3 and 7
 #Check whether the given number is divisible by 3 and 7
-numb=int(input("Enter a number:"))
+numb=float(input("Enter a number:"))
 if numb%3==0 and numb%7==0:
     print("The number",numb," is divisible by both 7 and 3!")
 else:
--- #largest of 3 numbers.py
+++ #largest of 3 numbers.py
@@ -1,14 +1,5 @@
 #largest of 3 numbers
-a=int(input("Enter the 1st no:"))
-b=int(input("Enter the 2st no:"))
-c=int(input("Enter the 3st no:"))
-if a>b:
-    if a>c:
-        print(a,"is the largest number")
-    else:
-        print(c,"is the largest number") 
-else:
-    if b>c:
-        print(b,"is the largest number")
-    else:
-        print(c,"is the largest number")
+a=float(input("Enter the 1st no:"))
+b=float(input("Enter the 2st no:"))
+c=float(input("Enter the 3st no:"))
+print("Maximum value :",max(a,b,c))
