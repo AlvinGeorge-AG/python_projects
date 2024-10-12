e8070171c1d2c19262600ae565ff10e7b419b766
Alvin george <alvingeorge_@outlook.com>
2024-10-12 20:16:59 +0530

    new_way

--- #Celsius (C), and Fahrenheit (F).py
+++ #Celsius (C), and Fahrenheit (F).py
@@ -1,4 +1,61 @@
 #Celsius (C), and Fahrenheit (F)
+print("Temperature converter")
+while True:
+    print("""
+            1=celsius to fahrenheit
+            2=fahrenheit to celsius
+            3=Exit""")
+    ask=int(input("Enter the choice :"))
+    if ask==1:
+        cel=float(input("Enter the temperature in degree celsius :"))
+        #C = (°F - 32) × 5/9] 
+        #so °F=cel(9/5)+32
+        fah=(cel*(9/5)+32)
+        print("Temperature in fahrenheit is ",fah)
+    elif ask==2:
+        #C = (°F - 32) × 5/9] 
+            far=float(input("Enter the temperature in degree fahrenheit :"))
+            cel=((far-32)*(5/9))
+            print("Temperature in celsius is ",cel)
+    elif ask==3:
+         break        
+    else:
+         print("Please enter valid choice")        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 check=input("Do you want to convert celsius to fahrenheit ,[y/n]")
 while check=='y'or check=='n':
     if check=='y':
@@ -20,4 +77,10 @@ while check!='y' or check!='n':
      break
 
             
+
+"""
+
+
+
+
 
