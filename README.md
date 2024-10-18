247f22e997d5acf6c428fc702a85f659125239dc
Alvin george <alvingeorge_@outlook.com>
2024-10-18 18:25:59 +0530

    ktu grade added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Some Basic Example Problems Using Python Language
+lab works for semester 1
---
+++ ktu grade and point calculator.py
@@ -0,0 +1,4 @@
+#A simple program to calculate ktu grade and points
+mark=int(input("Enter the marks out of 100 :"))
+percentage =((mark)/100)*100
+# print(percentage)
