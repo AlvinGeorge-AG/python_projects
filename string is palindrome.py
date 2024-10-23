36981c16a7a72463e9fce268ac3791ed773e2ba1
ALVIN GEORGE <Alvingeorge_@outlook.com>
2024-10-23 17:46:42 +0530

    Update  string is palindrome.py

--- string is palindrome.py
+++ string is palindrome.py
@@ -1,9 +1,9 @@
 #Check whether the given string is palindrome.( learn slicing )
 # z='MALAYALAM'
-# zz=z[::-1]
+# zp=z[::-1]
 y=input("Enter the string :")
-yy=y[::-1]
-if y==yy:
-    print("The give string,",y,"is a Palindrome!")
+yp=y[::-1]
+if y==yp:
+    print("The given string,",y," is a Palindrome!")
 else:
     print("The given string is not a Palindrome!")    
