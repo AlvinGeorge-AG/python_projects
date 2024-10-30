e45d0458081ff08a4cf052e4f9c9728913b7cc58
Alvin George <Alvingeorge_@outlook.com>
2024-10-30 16:04:44 +0530

    yy

--- cycle2/exp7.py
+++ cycle2/exp7.py
@@ -1,8 +1,11 @@
 #to find he sum of digits of a number
-y=0
 i=0
+x=0
 numb=input("enter a number :")
-while i in range(0,len(numb)+1):
-    y=y+int(i)
+y=list(numb)
+
+while i in range(0,len(y)):
+    x=x+int(y[i])
     i=i+1
-print(y)    
+print(x)    
+print(y)
