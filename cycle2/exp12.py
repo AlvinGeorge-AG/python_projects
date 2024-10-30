8ccb0e0f1f4a2f6cba20b270af74a9069e53c086
Alvin George <Alvingeorge_@outlook.com>
2024-10-30 15:48:04 +0530

    g

---
+++ cycle2/exp12.py
@@ -0,0 +1,9 @@
+#to count the number of vowel characters in a string
+string=input("Enter a string :")
+w=string.count('a')
+r=string.count('e')
+t=string.count('i')
+p=string.count('o')
+q=string.count('u')
+y=w+r+t+p+q
+print(y)
---
+++ cycle2/exp7.py
@@ -0,0 +1,8 @@
+#to find he sum of digits of a number
+y=0
+i=0
+numb=input("enter a number :")
+while i in range(0,len(numb)+1):
+    y=y+int(i)
+    i=i+1
+print(y)    
