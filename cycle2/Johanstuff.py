58bdc591d86b53e65ff8426cd029dfbaa7f25b29
Alvin George <Alvingeorge_@outlook.com>
2024-11-06 20:12:04 +0530

    new folder cycle1

--- cycle2/Johanstuff.py
+++
@@ -1,8 +0,0 @@
-l=[10,5,8,3,7,2]
-o=['a','f','l']
-# print(l.sort())
-# print(enumerate(l))
-# for index,value in enumerate(l):
-#     print(index,value)
-# for i in zip(o,l):
-#     print(i)
--- cycle2/exp14.py
+++ cycle2/exp14.py
@@ -1,3 +1,20 @@
 #to count the number of letters ,digits ,uppercase ,and lowercase letters in a string
 strinf=input("Enter a string :")
-a=strinf.count()
+no_cout=0
+no_upper=0
+no_lower=0
+no_letter=0
+no=0
+for i in strinf:
+    if i.isdigit():
+        no_cout+=1
+    elif i.isupper():
+        no_upper+=1    
+    elif i.islower():
+        no_lower+=1    
+    if i.isalpha():
+        no_letter+=1    
+print('Number of digits :',no_cout)  
+print("Number of Upper case Digits :",no_upper)  
+print('Number of lower case digits :',no_lower)
+print('Number of letters :',no_letter)
