16c77e7cb79812bdba22878de21ac5707cca7ea8
Alvin George <Alvingeorge_@outlook.com>
2024-11-06 18:47:05 +0530

    voweladdedremove

--- cycle2/exp13.py
+++ cycle2/exp13.py
@@ -1,15 +1,9 @@
-#to remove all vowel characters from a string
+# #to remove all vowel characters from a string
 stringf=(input("Enter a string :")).lower()
-y=list(stringf)
-# j=(stringf)
-# print(stringf)
-# print(y)
-for i in range(0,len(stringf)):
-    if y[i] in ['e','i','a','u','o']:
-        y.remove(y[i])
-    # if 'o' in y:
-    #     y.remove('o')  
-      
-newstr=''.join(y)
-print("Old string :",stringf)
-print("New string :",newstr)
+vow='aeiou'
+newstr=''   
+for i in range(len(stringf)):
+    if stringf[i] not in vow:
+        newstr+=stringf[i]
+print(newstr)        
+
