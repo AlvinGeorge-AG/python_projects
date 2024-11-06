0d10d5f6ce27ab0d8dcf0efdce58a7cef4cb5bf8
ALVIN GEORGE <Alvingeorge_@outlook.com>
2024-11-06 09:04:49 +0530

    Update exp12.py

--- cycle2/exp12.py
+++ cycle2/exp12.py
@@ -1,16 +1,9 @@
 #to count the number of vowel characters in a string
 string=input("Enter a string :")
-w=string.count('a')
-r=string.count('e')
-t=string.count('i')
-p=string.count('o')
-q=string.count('u')
-y=w+r+t+p+q
-print(y)
-# lt=[]
-# for i in range(0,len(string)):
-#     vowel=['a','e','i','o','u']
-#     if string[i] in vowel:
-#         w=(string).count(vowel[i])
-#         lt.append(w)
-# print(sum(lt))        
+vow="aeiou"
+count=0
+for i in string:
+    if i in vow:
+        count+=1
+
+
