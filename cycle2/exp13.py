5991effbe6445ce23060af9e48a496f3babb7888
Alvin George <Alvingeorge_@outlook.com>
2024-11-06 07:39:52 +0530

    latest

--- cycle2/exp13.py
+++ cycle2/exp13.py
@@ -1,12 +1,15 @@
 #to remove all vowel characters from a string
-stringf=input("Enter a string :")
+stringf=(input("Enter a string :")).lower()
 y=list(stringf)
-print(y)
-for j in y:
-    if j in ['e','i','a','u']:
-        y.remove(j)
-    if 'o' in y:
-        y.remove('o')        
+# j=(stringf)
+# print(stringf)
+# print(y)
+for i in range(0,len(stringf)):
+    if y[i] in ['e','i','a','u','o']:
+        y.remove(y[i])
+    # if 'o' in y:
+    #     y.remove('o')  
+      
 newstr=''.join(y)
 print("Old string :",stringf)
 print("New string :",newstr)
