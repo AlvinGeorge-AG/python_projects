0d3323fc84ed9d1e5b279154ef4abe5b897f988e
Alvin George <Alvingeorge_@outlook.com>
2024-11-23 13:41:56 +0530

    final submit of cycle 2

--- cycle2/exp10.py
+++ cycle2/exp10.py
@@ -4,7 +4,6 @@ no_cout=0
 no_upper=0
 no_lower=0
 no_letter=0
-no=0
 for i in strinf:
     if i.isdigit():
         no_cout+=1
@@ -13,8 +12,10 @@ for i in strinf:
     if i.islower():
         no_lower+=1    
     if i.isalpha():
-        no_letter+=1    
+        no_letter+=1
+w=(strinf.split(' '))    
 print('Number of digits :',no_cout)  
-print("Number of Upper case Digits :",no_upper)  
-print('Number of lower case digits :',no_lower)
+print("Number of Upper case letters :",no_upper)  
+print('Number of lower case letters :',no_lower)
 print('Number of letters :',no_letter)
+print('Number of words :',len(w))
