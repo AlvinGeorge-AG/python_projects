e88d53374fc9822c710eebf6f5c712ce74725523
Alvin George <Alvingeorge_@outlook.com>
2024-11-24 11:28:03 +0530

    started cycle4

---
+++ cycle4/Functions/1.py
@@ -0,0 +1,12 @@
+# to find the sum of  numbers in the range between upper and lower bound
+def sumf():
+    a=int(input("Enter the lower bound :"))
+    b=int(input("Enter the higher bound :"))
+    sum=0
+    for i in range(a+1,b):
+        sum+=i
+    return sum
+print(sumf()) 
+# sumf()   
+
+
