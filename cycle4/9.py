6d51c13964007244aedb5ea6d2311a8a498a6dc7
Alvin George <Alvingeorge_@outlook.com>
2024-11-28 19:18:13 +0530

    9!!!!!!

---
+++ cycle4/9.py
@@ -0,0 +1,13 @@
+#to find the  greatest common diviser of two numbers
+def gdc(a,b):
+    if a>b:
+        min=b
+        max=a
+    else:
+        min=a
+        max=b
+    if min==0:
+        return max
+    else:
+        return gdc(min,max%min)
+print(gdc(36,24))    
